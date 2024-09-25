36dd19ffc2f465670c1c20f6c45ee4066b9d347b
Inuka-G <156221307+Inuka-G@users.noreply.github.com>
2024-09-25 21:20:10 +0530

    Update Raffle.sol

--- Raffle.sol
+++ Raffle.sol
@@ -9,15 +9,34 @@ pragma solidity 0.8.19;
  *
  */
 contract Raffel {
+    error Raffel_notEnoughEth();
     uint256 private immutable i_ticketFee;
+    uint256 private immutable i_intervalinSeconds;
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players; // address array payable
+    event RaffelEntered(address indexed player);
 
-    constructor(uint ticketFee) {
+    constructor(uint ticketFee, uint256 intervalinSeconds) {
         i_ticketFee = ticketFee;
+        i_intervalinSeconds = intervalinSeconds;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {}
-    function pickWinner() public {}
+    function enterRaffle() external payable {
+        if (msg.value < i_ticketFee) {
+            revert Raffel_notEnoughEth();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffelEntered(msg.sender);
+    }
+
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_intervalinSeconds) {
+            revert();
+        }
+    }
+
     function getTicketFee() external view returns (uint256) {
         return i_ticketFee;
-    }   
+    }
 }
